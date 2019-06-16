d587c9998d02e81df6c16e3149b4abb845dc4916
cocoasuny <cocoasuny@163.com>
2019-06-16 23:44:49 +0800

    1、移植mesh控制部分，未调试

--- Mesh_client_nrf52832_MDK/source/Application/inc/app_mesh_implement.h
+++ Mesh_client_nrf52832_MDK/source/Application/inc/app_mesh_implement.h
@@ -17,6 +17,80 @@
 #ifndef __APP_MESH_IMPLEMENT__
 #define __APP_MESH_IMPLEMENT__
 
+#include "sdk_config.h"
+
+
+/** Number of active servers.
+ * Note: If the value of SERVER_NODE_COUNT is increased, you may need to scale up the the replay
+ * protection list size (@ref REPLAY_CACHE_ENTRIES), by the appropriate amount, for the provisioner and
+ * client examples. For the provisioner example to work as expected, its replay protection list size should
+ * be greater than or equal to the total number of nodes it is going to configure after provisioning them.
+ * The replay protection list size of the client example should be greater than or equal to the total
+ * number of unicast addresses in the network that it can receive a message from.
+ */
+#define SERVER_NODE_COUNT (30)
+#if SERVER_NODE_COUNT > 30
+#error Maximum 30 servers currently supported by client example.
+#endif
+
+/** Number of active clients nodes. */
+#define CLIENT_NODE_COUNT            (1)
+
+/** Number of On-Off client models on the Switch Node */
+#define CLIENT_MODEL_INSTANCE_COUNT  (2)
+
+/** Number of group address being used in this example */
+#define GROUP_ADDR_COUNT             (2)
+
+/** Static authentication data */
+#define STATIC_AUTH_DATA {0x6E, 0x6F, 0x72, 0x64, 0x69, 0x63, 0x5F, 0x65, 0x78, 0x61, 0x6D, 0x70, 0x6C, 0x65, 0x5F, 0x31}
+
+/** UUID prefix length */
+#define NODE_UUID_PREFIX_LEN         (4)
+
+/** UUID prefix length */
+#define COMMON_UUID_PREFIX_LEN       (3)
+
+/** Common UUID prefix for client nodes */
+#define COMMON_CLIENT_UUID              0x00, 0x59, 0xCC
+
+/** Light switch client UUID */
+#define CLIENT_NODE_UUID_PREFIX         {COMMON_CLIENT_UUID, 0xAA}
+
+/** Light switch level client UUID */
+#define LEVEL_CLIENT_NODE_UUID_PREFIX   {COMMON_CLIENT_UUID, 0xBB}
+
+/** Common UUID prefix for server nodes */
+#define COMMON_SERVER_UUID              0x00, 0x59, 0x55
+
+/** UUID prefix for other nodes */
+#define SERVER_NODE_UUID_PREFIX         {COMMON_SERVER_UUID, 0xAA}
+
+/** UUID prefix for other level server nodes */
+#define LEVEL_SERVER_NODE_UUID_PREFIX   {COMMON_SERVER_UUID, 0xBB}
+
+/**
+ * Clock configuration for Nordic development boards.
+ */
+#if defined(S110)
+    #define DEV_BOARD_LF_CLK_CFG  NRF_CLOCK_LFCLKSRC_XTAL_20_PPM
+#elif NRF_SD_BLE_API_VERSION >= 5
+    #define DEV_BOARD_LF_CLK_CFG  { \
+        .source = NRF_SDH_CLOCK_LF_SRC, \
+        .rc_ctiv = NRF_SDH_CLOCK_LF_RC_CTIV, \
+        .rc_temp_ctiv = NRF_SDH_CLOCK_LF_RC_TEMP_CTIV, \
+        .accuracy = NRF_SDH_CLOCK_LF_ACCURACY \
+    }
+#else
+    #define DEV_BOARD_LF_CLK_CFG  { \
+        .source = NRF_CLOCK_LF_SRC_XTAL, \
+        .rc_ctiv = 0, \
+        .rc_temp_ctiv = 0, \
+        .xtal_accuracy = NRF_CLOCK_LF_XTAL_ACCURACY_20_PPM \
+    }
+#endif
+
+
 
 /* function declares */
 void ble_mesh_top_thread(void * arg);
--- Mesh_client_nrf52832_MDK/source/Application/src/app_mesh_implement.c
+++ Mesh_client_nrf52832_MDK/source/Application/src/app_mesh_implement.c
@@ -16,8 +16,50 @@
 /* Includes ------------------------------------------------------------------*/
 #include "app_freertos.h"
 #include "ble_softdevice_support.h"
+#include "app_mesh_implement.h"
+#include "nrf_mesh_config_core.h"
+#include "nrf_mesh_configure.h"
+#include "nrf_mesh_defines.h"
+#include "nrf_mesh.h"
+#include "mesh_stack.h"
+#include "mesh_provisionee.h"
+#include "mesh_app_utils.h"
+#include "generic_onoff_client.h"
+#include "platform.h"
 
 
+#define APP_STATE_OFF                (0)
+#define APP_STATE_ON                 (1)
+
+#define APP_UNACK_MSG_REPEAT_COUNT   (2)
+
+/* Private variable define ---------------------------------------------------*/
+static generic_onoff_client_t m_clients[CLIENT_MODEL_INSTANCE_COUNT];
+static bool                   m_device_provisioned;
+
+/* Private function declare --------------------------------------------------*/
+static void mesh_init(void);
+static void models_init_cb(void);
+static void app_gen_onoff_client_publish_interval_cb(access_model_handle_t handle, void * p_self);
+static void app_generic_onoff_client_status_cb(const generic_onoff_client_t * p_self,
+                                               const access_message_rx_meta_t * p_meta,
+                                               const generic_onoff_status_params_t * p_in);
+static void app_gen_onoff_client_transaction_status_cb(access_model_handle_t model_handle,
+                                                       void * p_args,
+                                                       access_reliable_status_t status);
+static void node_reset(void);													  
+static void config_server_evt_cb(const config_server_evt_t * p_evt);
+static void provisioning_complete_cb(void);
+static void device_identification_start_cb(uint8_t attention_duration_s);	
+static void provisioning_aborted_cb(void);				
+static void start(void);													   
+													   
+const generic_onoff_client_callbacks_t client_cbs =
+{
+    .onoff_status_cb = app_generic_onoff_client_status_cb,
+    .ack_transaction_status_cb = app_gen_onoff_client_transaction_status_cb,
+    .periodic_publish_cb = app_gen_onoff_client_publish_interval_cb
+};													   
 
 /**@brief Thread for handling the ble mesh.
  *
@@ -32,23 +74,187 @@ void ble_mesh_top_thread(void * arg)
 	
 	 ble_stack_init();
 	
+#if MESH_FEATURE_GATT_ENABLED
+    gap_params_init();
+    conn_params_init();
+#endif
+
+    mesh_init();	
+	start();
 	
 	while(1)
 	{
-	
+		(void)sd_app_evt_wait();
 	}
 }
 
+/**
+  * @brief  mesh_init
+  * @note   Function for initializing the mesh
+  * @param  None
+  * @retval None
+  */
+static void mesh_init(void)
+{
+    uint8_t dev_uuid[NRF_MESH_UUID_SIZE];
+    uint8_t node_uuid_prefix[NODE_UUID_PREFIX_LEN] = CLIENT_NODE_UUID_PREFIX;
+
+    ERROR_CHECK(mesh_app_uuid_gen(dev_uuid, node_uuid_prefix, NODE_UUID_PREFIX_LEN));
+    mesh_stack_init_params_t init_params =
+    {
+        .core.irq_priority       = NRF_MESH_IRQ_PRIORITY_LOWEST,
+        .core.lfclksrc           = DEV_BOARD_LF_CLK_CFG,
+        .core.p_uuid             = dev_uuid,
+        .models.models_init_cb   = models_init_cb,
+        .models.config_server_cb = config_server_evt_cb
+    };
+    ERROR_CHECK(mesh_stack_init(&init_params, &m_device_provisioned));
+}
 
 
+static void start(void)
+{
+    if (!m_device_provisioned)
+    {
+        static const uint8_t static_auth_data[NRF_MESH_KEY_SIZE] = STATIC_AUTH_DATA;
+        mesh_provisionee_start_params_t prov_start_params =
+        {
+            .p_static_data    = static_auth_data,
+            .prov_complete_cb = provisioning_complete_cb,
+            .prov_device_identification_start_cb = device_identification_start_cb,
+            .prov_device_identification_stop_cb = NULL,
+            .prov_abort_cb = provisioning_aborted_cb,
+            .p_device_uri = NULL
+        };
+        ERROR_CHECK(mesh_provisionee_prov_start(&prov_start_params));
+    }
 
+    const uint8_t *p_uuid = nrf_mesh_configure_device_uuid_get();
+    UNUSED_VARIABLE(p_uuid);
+    __LOG_XB(LOG_SRC_APP, LOG_LEVEL_INFO, "Device UUID ", p_uuid, NRF_MESH_UUID_SIZE);
 
+    ERROR_CHECK(mesh_stack_start());
 
+    //hal_led_mask_set(LEDS_MASK, LED_MASK_STATE_OFF);
+    //hal_led_blink_ms(LEDS_MASK, LED_BLINK_INTERVAL_MS, LED_BLINK_CNT_START);
+}
 
+static void device_identification_start_cb(uint8_t attention_duration_s)
+{
+    /*hal_led_mask_set(LEDS_MASK, false);
+    hal_led_blink_ms(BSP_LED_2_MASK  | BSP_LED_3_MASK, 
+                     LED_BLINK_ATTENTION_INTERVAL_MS, 
+                     LED_BLINK_ATTENTION_COUNT(attention_duration_s));
+    */
+}
 
+static void provisioning_aborted_cb(void)
+{
+    //hal_led_blink_stop();
+}
 
+static void provisioning_complete_cb(void)
+{
+    __LOG(LOG_SRC_APP, LOG_LEVEL_INFO, "Successfully provisioned\n");
 
+#if MESH_FEATURE_GATT_ENABLED
+    /* Restores the application parameters after switching from the Provisioning
+     * service to the Proxy  */
+    gap_params_init();
+    conn_params_init();
+#endif
 
+    dsm_local_unicast_address_t node_address;
+    dsm_local_unicast_addresses_get(&node_address);
+    __LOG(LOG_SRC_APP, LOG_LEVEL_INFO, "Node Address: 0x%04x \n", node_address.address_start);
+
+    //hal_led_blink_stop();
+    //hal_led_mask_set(LEDS_MASK, LED_MASK_STATE_OFF);
+    //hal_led_blink_ms(LEDS_MASK, LED_BLINK_INTERVAL_MS, LED_BLINK_CNT_PROV);
+}
+
+/* This callback is called periodically if model is configured for periodic publishing */
+static void app_gen_onoff_client_publish_interval_cb(access_model_handle_t handle, void * p_self)
+{
+     __LOG(LOG_SRC_APP, LOG_LEVEL_WARN, "Publish desired message here.\n");
+}
+
+/* Acknowledged transaction status callback, if acknowledged transfer fails, application can
+* determine suitable course of action (e.g. re-initiate previous transaction) by using this
+* callback.
+*/
+static void app_gen_onoff_client_transaction_status_cb(access_model_handle_t model_handle,
+                                                       void * p_args,
+                                                       access_reliable_status_t status)
+{
+    switch(status)
+    {
+        case ACCESS_RELIABLE_TRANSFER_SUCCESS:
+            __LOG(LOG_SRC_APP, LOG_LEVEL_INFO, "Acknowledged transfer success.\n");
+            break;
+
+        case ACCESS_RELIABLE_TRANSFER_TIMEOUT:
+            //hal_led_blink_ms(LEDS_MASK, LED_BLINK_SHORT_INTERVAL_MS, LED_BLINK_CNT_NO_REPLY);
+            __LOG(LOG_SRC_APP, LOG_LEVEL_INFO, "Acknowledged transfer timeout.\n");
+            break;
+
+        case ACCESS_RELIABLE_TRANSFER_CANCELLED:
+            __LOG(LOG_SRC_APP, LOG_LEVEL_INFO, "Acknowledged transfer cancelled.\n");
+            break;
+
+        default:
+            ERROR_CHECK(NRF_ERROR_INTERNAL);
+            break;
+    }
+}
+
+/* Generic OnOff client model interface: Process the received status message in this callback */
+static void app_generic_onoff_client_status_cb(const generic_onoff_client_t * p_self,
+                                               const access_message_rx_meta_t * p_meta,
+                                               const generic_onoff_status_params_t * p_in)
+{
+    if (p_in->remaining_time_ms > 0)
+    {
+        __LOG(LOG_SRC_APP, LOG_LEVEL_INFO, "OnOff server: 0x%04x, Present OnOff: %d, Target OnOff: %d, Remaining Time: %d ms\n",
+              p_meta->src.value, p_in->present_on_off, p_in->target_on_off, p_in->remaining_time_ms);
+    }
+    else
+    {
+        __LOG(LOG_SRC_APP, LOG_LEVEL_INFO, "OnOff server: 0x%04x, Present OnOff: %d\n",
+              p_meta->src.value, p_in->present_on_off);
+    }
+}
+
+static void node_reset(void)
+{
+    __LOG(LOG_SRC_APP, LOG_LEVEL_INFO, "----- Node reset  -----\n");
+    //hal_led_blink_ms(LEDS_MASK, LED_BLINK_INTERVAL_MS, LED_BLINK_CNT_RESET);
+    /* This function may return if there are ongoing flash operations. */
+    mesh_stack_device_reset();
+}
+
+static void config_server_evt_cb(const config_server_evt_t * p_evt)
+{
+    if (p_evt->type == CONFIG_SERVER_EVT_NODE_RESET)
+    {
+        node_reset();
+    }
+}
+
+static void models_init_cb(void)
+{
+    __LOG(LOG_SRC_APP, LOG_LEVEL_INFO, "Initializing and adding models\n");
+
+    for (uint32_t i = 0; i < CLIENT_MODEL_INSTANCE_COUNT; ++i)
+    {
+        m_clients[i].settings.p_callbacks = &client_cbs;
+        m_clients[i].settings.timeout = 0;
+        m_clients[i].settings.force_segmented = APP_CONFIG_FORCE_SEGMENTATION;
+        m_clients[i].settings.transmic_size = APP_CONFIG_MIC_SIZE;
+
+        ERROR_CHECK(generic_onoff_client_init(&m_clients[i], i + 1));
+    }
+}
 
 
 
